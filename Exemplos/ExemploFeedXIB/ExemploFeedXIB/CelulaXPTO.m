d796b1f8451f9e0251da6bc8831a78dd21f9621f
Pedro Henrique Ferreira Figueiredo <intellitour@gmail.com>
2016-11-09 17:46:26 -0200

    Atualização do exemplo Feed com XIB

---
+++ Exemplos/ExemploFeedXIB/ExemploFeedXIB/CelulaXPTO.m
@@ -0,0 +1,30 @@
+//
+//  CelulaXPTO.m
+//  ExemploFeedXIB
+//
+//  Created by Pedro Henrique on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import "CelulaXPTO.h"
+
+@interface CelulaXPTO ()
+
+
+
+@end
+
+@implementation CelulaXPTO
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
